19b6828a93f980453809e4acc8760304277e8172
Indra Mubarok <indramubarok@Indras-MacBook-Pro.local>
2021-05-18 13:25:08 +0700

    maul

--- android/app/src/main/java/com/sellerapp/MainActivity.java
+++ android/app/src/main/java/com/sellerapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SellerApp";
+    return "RockStationBanjarmasin";
   }
 }
